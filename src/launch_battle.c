9ba7b56bbb1c5b63e74ddec74db6d5aab2dad1ad
Luka Camus <luka.camus@epitech.eu>
2023-04-24 18:00:13 +0200

    [MODIFY] somewhat functionnal algorithm

--- src/launch_battle.c
+++ src/launch_battle.c
@@ -11,38 +11,41 @@
 #include <semaphore.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-static panoramix_t *pano;
+static druid_t druid = {0};
+static villager_t *villagers = NULL;
 static pthread_mutex_t potion;
-
-// static sem_t villagers;
+static sem_t *sem;
+static bool to_wake = false;
 
 static void *villager_battling(void *_ind)
 {
     size_t ind = (size_t)_ind - 1;
 
     printf(VILLAGER_START, ind);
-    while (pano->villagers[ind].fights_left > 0) {
-        if (pano->villagers[ind].fighting) {
-            pano->villagers[ind].fights_left -= 1;
-            printf(VILLAGER_FIGHTING, ind, pano->villagers[ind].fights_left);
-            pano->villagers[ind].fighting = false;
+    sem_post(sem);
+    while (villagers[ind].fights_left > 0) {
+        if (villagers[ind].fighting) {
+            villagers[ind].fights_left -= 1;
+            printf(VILLAGER_FIGHTING, ind, villagers[ind].fights_left);
+            villagers[ind].fighting = false;
             continue;
         }
-        pthread_mutex_trylock(&potion);
-        printf(VILLAGER_POTION, ind, pano->druid.pot_left);
-        if (pano->druid.pot_left > 0) {
-            pano->druid.pot_left -= 1;
-            pano->villagers[ind].fighting = true;
-        } else if (pano->druid.nb_refills > 0) {
+        pthread_mutex_lock(&potion);
+        if (druid.pot_left > 0) {
+            printf(VILLAGER_POTION, ind, druid.pot_left);
+            druid.pot_left -= 1;
+            villagers[ind].fighting = true;
+        } else if (druid.nb_refills > 0 && druid.sleeping && !to_wake) {
+            printf(VILLAGER_POTION, ind, druid.pot_left);
             printf(VILLAGER_CALL, ind);
-            pano->druid.nb_refills -= 1;
-            pano->druid.pot_left = pano->druid.pot_size;
-            printf(DRUID_REFILL, pano->druid.nb_refills);
-        } else
-            break;
+            to_wake = true;
+        }
         pthread_mutex_unlock(&potion);
     }
+    sem_wait(sem);
     printf(VILLAGER_SLEEP, ind);
     return NULL;
 }
@@ -50,9 +53,20 @@ static void *villager_battling(void *_ind)
 static void *druid_sleeping(void __attribute__((unused)) * data)
 {
     printf(DRUID_START);
-    while (pano->druid.nb_refills > 0 || pano->druid.pot_left > 0) {
-        if (!villagers_fighting(pano->villagers))
+    while (druid.nb_refills > 0 || druid.pot_left > 0) {
+        if (!villagers_fighting(villagers))
             break;
+        pthread_mutex_lock(&potion);
+        if (to_wake && druid.pot_left == 0 && druid.nb_refills > 0) {
+            druid.pot_left = druid.pot_size;
+            druid.nb_refills -= 1;
+            printf(DRUID_REFILL, druid.nb_refills);
+            to_wake = false;
+            druid.sleeping = false;
+        }
+        if (druid.pot_left == 0)
+            druid.sleeping = true;
+        pthread_mutex_unlock(&potion);
     }
     printf(DRUID_SLEEP);
     return NULL;
@@ -62,16 +76,20 @@ int launch_battle(panoramix_t *infos)
 {
     pthread_t tid[infos ? infos->nb_villagers + 1 : 0];
 
-    if (!(pano = infos))
+    if (!infos || !(sem = malloc(sizeof(sem_t))))
         return EPI_FAILURE;
+    druid = infos->druid;
+    villagers = infos->villagers;
     pthread_mutex_init(&potion, NULL);
+    sem_init(sem, 0, 0);
     if (pthread_create(&tid[0], NULL, &druid_sleeping, NULL) < 0)
         return EPI_FAILURE;
-    for (size_t i = 1; i <= pano->nb_villagers; i++)
+    for (size_t i = 1; i <= infos->nb_villagers; i++)
         if (pthread_create(&tid[i], NULL, &villager_battling, (void *)i) < 0)
             return EPI_FAILURE;
-    for (size_t i = 0; i <= pano->nb_villagers; i++)
+    for (size_t i = 0; i <= infos->nb_villagers; i++)
         pthread_join(tid[i], NULL);
     pthread_mutex_destroy(&potion);
+    sem_destroy(sem);
     return EPI_SUCCESS;
 }
